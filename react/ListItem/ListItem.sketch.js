b4d7cedc5abc347d26b7f3b9467a5bcc22e1d07c
Chi Wong <chiwong@seekasia.com>
2018-05-30 17:57:14 +0800

    feat(listitem): code tidy

--- react/ListItem/ListItem.sketch.js
+++ react/ListItem/ListItem.sketch.js
@@ -7,11 +7,10 @@ import Badge from '../Badge/Badge';
 export const listItem = {
 
   'Value': <ListItem value='Description' />,
-  'title, value': <ListItem title='Title' value="Description"/>,
+  'title, value': <ListItem title='Title' value="Description" />,
   'title, value, icon': <ListItem title='Qualification' value='Degree' icon={<IconEducation />} />,
-  'title, value, icon, badge': <ListItem badge={<Badge color="default" isBold label="Badge"/>} icon={<IconEducation />} title="Job Posted Date" value="10 May 2018"/>,
-  'title, value, icon, badge(diff color)': <ListItem badge={<Badge color="progressing" isBold label="Badge"/>} icon={<IconEducation />} title="Job Posted Date" value="10 May 2018"/>
-  
+  'title, value, icon, badge': <ListItem badge={<Badge color="default" isBold label="Badge" />} icon={<IconEducation />} title="Job Posted Date" value="10 May 2018" />,
+  'title, value, icon, badge(diff color)': <ListItem badge={<Badge color="progressing" isBold label="Badge" />} icon={<IconEducation />} title="Job Posted Date" value="10 May 2018" />
 };
 
 // Export text styles as symbols
