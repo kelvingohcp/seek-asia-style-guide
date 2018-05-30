d34e2a17a83f8709b2714c6c7bdb3c4e96222d2d
Syakir Uzair <syakiru@seekasia.com>
2018-05-30 11:14:29 +0800

    feat(grid-js): Add grid.js

---
+++ theme/layout/grid.js
@@ -0,0 +1,8 @@
+export const gridBase = 4;
+
+export const gridRowHeight = 4;
+export const gridGutterWidth = gridBase * 4;
+
+// Slated for deletion
+export const gridColumnWidth = 60;
+export const gridContainerWidth = 940;
