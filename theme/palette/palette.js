d78fbef8df7b02c8941f05d5a57034abfd876ce8
Syakir Uzair <syakiru@seekasia.com>
2018-05-30 13:53:55 +0800

    feat(grid-js): Fix linting

--- theme/palette/palette.js
+++ theme/palette/palette.js
@@ -1,9 +1,9 @@
-//brand colors
+// brand colors
 export const dbBlue 		= '#0C4B85';
 export const jsBlue 		= '#1C3F94';
 export const saBlue 		= '#0D3880';
 
-//Action colors
+// Action colors
 export const actionYellow 		= '#FFF200';
 export const actionOrange 		= '#FF9000';
 export const actionBlue 		= '#298EB9';
@@ -11,7 +11,7 @@ export const actionGreen 		= '#63B209';
 export const actionRed 			= '#FF4B4B';
 export const actionPastelBlue	= '#DDF1FA';
 
-//Neutrals
+// Neutrals
 export const saBlack 		= '#000000';
 export const saGrey1		= '#333333';
 export const saGrey2		= '#666666';
@@ -21,4 +21,4 @@ export const saGrey5		= '#EEEEEE';
 export const saWhite 		= '#FFFFFF';
 
 export const linkBlue = '#2E4191';
-export const panelPink  = '#FF0072';
+export const panelPink = '#FF0072';
