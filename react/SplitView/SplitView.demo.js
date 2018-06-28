8d39c19aae04b6e9af73ded91f6b21bd75916c75
PepeFranco <gfranco@occ.com.mx>
2018-06-28 17:27:09 +1000

    fix(splitView): Added missing files

---
+++ react/SplitView/SplitView.demo.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import SplitView from './SplitView';
+import { Card, Section, Text } from 'seek-asia-style-guide/react';
+
+export default {
+  route: '/split-view',
+  title: 'Split View',
+  component: SplitView,
+  initialProps: {
+    children: [
+      <Card key="1">
+        <Section>
+          <Text shouting>Left Content</Text>
+          <Text>
+            This content is provided by the first child. It will take the whole
+            container on mobile and tablet.It has 2 different sizes on desktop
+            and large screen.
+          </Text>
+        </Section>
+      </Card>,
+      <Card key="2">
+        <Section>
+          <Text shouting>Right Content</Text>
+          <Text>
+            This content is provided by the second child. It will not show on
+            mobile or tablet. This has a maximum width regardless of screen
+            size.
+          </Text>
+        </Section>
+      </Card>
+    ]
+  },
+  options: []
+};
