6ab905b6773a86164b4923d18bdd8f6f8d9b5dd5
PepeFranco <gfranco@occ.com.mx>
2018-07-04 11:01:56 +1000

    feat(SplitView): Added height prop

--- react/SplitView/SplitView.js
+++ react/SplitView/SplitView.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import styles from './SplitView.less';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
-export default function SplitView({ children }) {
+export default function SplitView({ children, height }) {
+  const heightStyle = height ? { height } : null;
   return (
     <div className={styles.root}>
       {children &&
         children.length > 0 && (
-        <div className={styles.leftChild}>{children[0]}</div>
+        <div className={styles.leftChild} style={{height}}>
+          {children[0]}
+        </div>
       )}
       {children &&
         children.length > 1 && (
-        <div className={styles.rightChild}>{children.slice(1)}</div>
+        <div className={styles.rightChild} style={heightStyle}>
+          {children.slice(1)}
+        </div>
       )}
     </div>
   );
 }
 
 SplitView.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  height: PropTypes.int
 };
