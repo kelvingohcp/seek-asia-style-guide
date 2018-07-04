ceb655292bb31fef8f05b5daed946fecb8c1904b
PepeFranco <gfranco@occ.com.mx>
2018-07-04 11:03:39 +1000

    fix(SplitView): Added tests

---
+++ react/SplitView/SplitView.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import SplitView from './SplitView';
+
+const renderSplitView = props =>
+  shallow(
+    <SplitView {...props}>
+      <div> Child 1</div>
+      <div> Child 2</div>
+    </SplitView>
+  );
+
+describe('Split View:', () => {
+  it('should render with default props', () => {
+    expect(renderSplitView()).toMatchSnapshot();
+  });
+
+  it('should render with height prop', () => {
+    expect(renderSplitView({ height: 200 })).toMatchSnapshot();
+  });
+
+});
