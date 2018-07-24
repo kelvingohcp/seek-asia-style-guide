2c90153a18b61a3cbe5ad8d1a79dedf2f5ce771c
Jun Kit <jkchan@seekasia.com>
2018-07-24 17:46:44 +0800

    Feature/update title card (#165)
    ### Features
    
    * **Job Title:** update the font size and using individual padding

--- react/TitledCard/TitledCard.js
+++ react/TitledCard/TitledCard.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './TitledCard.less';
-import { Card, Section, Text } from 'seek-asia-style-guide/react';
+import { Card, Text } from 'seek-asia-style-guide/react';
 
 export default function TitledCard({ children, text, ...restProps }) {
   return (
     <Card {...restProps}>
-      <Section>
-        <Text yelling strong>{text}</Text>
+      <div className={styles.container}>
+        <Text waving>{text}</Text>
         <hr className={styles.line} />
         {children}
-      </Section>
+      </div>
     </Card>
   );
 }
