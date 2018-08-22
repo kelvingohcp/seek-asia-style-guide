69ad65cec3b02da429247ff412e65105fb4194fe
Swetha Rajendra <swethar@seekasia.com>
2018-08-22 12:09:40 +0800

    Remove shadow footer (#200)
    fix(remove-shadow-footer): remove the shadow on footer

--- jobsDB/Footer/FooterHK/components/PartnerSites/PartnerSites.js
+++ jobsDB/Footer/FooterHK/components/PartnerSites/PartnerSites.js
@@ -15,59 +15,59 @@ const PartnerSites = ({ messages, externalLink }) => {
       chevronAlignment='center'>
 
       {externalLink(
-        <ListItem value='BabaJob (India)' disableBackground compact className={styles.partners} descriptionProps={{ semiStrong: true, whistling: true }} />,
+        <ListItem value='BabaJob (India)' disableBackground compact noShadow descriptionProps={{ semiStrong: true, whistling: true }} />,
         messages['FooterHK.BabaJobLink']
       )}
       {externalLink(
-        <ListItem value='Bdjobs (Bandladesh)' disableBackground compact className={styles.partners} descriptionProps={{ semiStrong: true, whistling: true }} />,
+        <ListItem value='Bdjobs (Bandladesh)' disableBackground compact noShadow descriptionProps={{ semiStrong: true, whistling: true }} />,
         messages['FooterHK.BdjobsLink']
       )}
       {externalLink(
-        <ListItem value='Brighter Monday (East Africa)' disableBackground compact className={styles.partners} descriptionProps={{ semiStrong: true, whistling: true }} />,
+        <ListItem value='Brighter Monday (East Africa)' disableBackground compact noShadow descriptionProps={{ semiStrong: true, whistling: true }} />,
         messages['FooterHK.BrighterMondayLink']
       )}
       {externalLink(
-        <ListItem value='Catho (Brazil)' disableBackground compact className={styles.partners} descriptionProps={{ semiStrong: true, whistling: true }} />,
+        <ListItem value='Catho (Brazil)' disableBackground compact noShadow descriptionProps={{ semiStrong: true, whistling: true }} />,
         messages['FooterHK.CathoLink']
       )}
       {externalLink(
-        <ListItem value='Jobberman (West Africa)' disableBackground compact className={styles.partners} descriptionProps={{ semiStrong: true, whistling: true }} />,
+        <ListItem value='Jobberman (West Africa)' disableBackground compact noShadow descriptionProps={{ semiStrong: true, whistling: true }} />,
         messages['FooterHK.JobbermanLink']
       )}
       {externalLink(
-        <ListItem value='JobStreet (S.E. Asia)' disableBackground compact className={styles.partners} descriptionProps={{ semiStrong: true, whistling: true }} />,
+        <ListItem value='JobStreet (S.E. Asia)' disableBackground compact noShadow descriptionProps={{ semiStrong: true, whistling: true }} />,
         messages['FooterHK.JobStreetLink']
       )}
       {externalLink(
-        <ListItem value='Jora (Hong Kong)' disableBackground compact className={styles.partners} descriptionProps={{ semiStrong: true, whistling: true }} />,
+        <ListItem value='Jora (Hong Kong)' disableBackground compact noShadow descriptionProps={{ semiStrong: true, whistling: true }} />,
         messages['FooterHK.JoraLink']
       )}
       {externalLink(
-        <ListItem value='Manager (Brazil)' disableBackground compact className={styles.partners} descriptionProps={{ semiStrong: true, whistling: true }} />,
+        <ListItem value='Manager (Brazil)' disableBackground compact noShadow descriptionProps={{ semiStrong: true, whistling: true }} />,
         messages['FooterHK.ManagerLink']
       )}
       {externalLink(
-        <ListItem value='OCC Mundial (Mexico)' disableBackground compact className={styles.partners} descriptionProps={{ semiStrong: true, whistling: true }} />,
+        <ListItem value='OCC Mundial (Mexico)' disableBackground compact noShadow descriptionProps={{ semiStrong: true, whistling: true }} />,
         messages['FooterHK.OCCLink']
       )}
       {externalLink(
-        <ListItem value='Seaman Jobsite (Philippines)' disableBackground compact className={styles.partners} descriptionProps={{ semiStrong: true, whistling: true }} />,
+        <ListItem value='Seaman Jobsite (Philippines)' disableBackground compact noShadow descriptionProps={{ semiStrong: true, whistling: true }} />,
         messages['FooterHK.SeamanJobsiteLink']
       )}
       {externalLink(
-        <ListItem value='SEEK (Australia)' disableBackground compact className={styles.partners} descriptionProps={{ semiStrong: true, whistling: true }} />,
+        <ListItem value='SEEK (Australia)' disableBackground compact noShadow descriptionProps={{ semiStrong: true, whistling: true }} />,
         messages['FooterHK.SEEKLink']
       )}
       {externalLink(
-        <ListItem value='Work Abroad (Philippines)' disableBackground compact className={styles.partners} descriptionProps={{ semiStrong: true, whistling: true }} />,
+        <ListItem value='Work Abroad (Philippines)' disableBackground compact noShadow descriptionProps={{ semiStrong: true, whistling: true }} />,
         messages['FooterHK.WorkAbroadLink']
       )}
       {externalLink(
-        <ListItem value='Workana' disableBackground compact className={styles.partners} descriptionProps={{ semiStrong: true, whistling: true }} />,
+        <ListItem value='Workana' disableBackground compact noShadow descriptionProps={{ semiStrong: true, whistling: true }} />,
         messages['FooterHK.WorkanaLink']
       )}
       {externalLink(
-        <ListItem value='Zhaopin (China)' disableBackground compact className={styles.partners} descriptionProps={{ semiStrong: true, whistling: true }} />,
+        <ListItem value='Zhaopin (China)' disableBackground compact noShadow descriptionProps={{ semiStrong: true, whistling: true }} />,
         messages['FooterHK.ZhaopinLink']
       )}
     </DropdownList>
