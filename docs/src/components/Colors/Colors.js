948c51da14a30e86f46c11c011038c150d3ea438
Patrick Liu <patrickliu@seekasia.com>
2018-08-22 11:19:30 +0800

    feat(color): Add blue shade examples in color page (#197)

--- docs/src/components/Colors/Colors.js
+++ docs/src/components/Colors/Colors.js
@@ -98,6 +98,44 @@ const jsSwatches = [
   }
 ];
 
+const blueSwatches = [
+  {
+    label: 'Blue 1',
+    classVar: 'Blue1',
+    cssVar: '@saBlue1',
+    textClass: styles.colorBlue1,
+    bgClass: styles.backgroundBlue1
+  },
+  {
+    label: 'Blue 2',
+    classVar: 'Blue2',
+    cssVar: '@saBlue2',
+    textClass: styles.colorBlue2,
+    bgClass: styles.backgroundBlue2
+  },
+  {
+    label: 'Blue 3',
+    classVar: 'Blue3',
+    cssVar: '@saBlue3',
+    textClass: styles.colorBlue3,
+    bgClass: styles.backgroundBlue3
+  },
+  {
+    label: 'Blue 4',
+    classVar: 'Blue4',
+    cssVar: '@saBlue4',
+    textClass: styles.colorBlue4,
+    bgClass: styles.backgroundBlue4
+  },
+  {
+    label: 'Blue 5',
+    classVar: 'Blue5',
+    cssVar: '@saBlue5',
+    textClass: styles.colorBlue5,
+    bgClass: styles.backgroundBlue5
+  }
+];
+
 const greySwatches = [
   {
     label: 'Black',
@@ -107,42 +145,42 @@ const greySwatches = [
     bgClass: styles.backgroundBlack
   },
   {
-    label: 'Grey #1',
+    label: 'Grey 1',
     classVar: 'Grey1',
     cssVar: '@saGrey1',
     textClass: styles.colorGrey1,
     bgClass: styles.backgroundGrey1
   },
   {
-    label: 'Grey #2',
+    label: 'Grey 2',
     classVar: 'Grey2',
     cssVar: '@saGrey2',
     textClass: styles.colorGrey2,
     bgClass: styles.backgroundGrey2
   },
   {
-    label: 'Grey #3',
+    label: 'Grey 3',
     classVar: 'Grey3',
     cssVar: '@saGrey3',
     textClass: styles.colorGrey3,
     bgClass: styles.backgroundGrey3
   },
   {
-    label: 'Grey #4',
+    label: 'Grey 4',
     classVar: 'Grey4',
     cssVar: '@saGrey4',
     textClass: styles.colorGrey4,
     bgClass: styles.backgroundGrey4
   },
   {
-    label: 'Grey #5',
+    label: 'Grey 5',
     classVar: 'Grey5',
     cssVar: '@saGrey5',
     textClass: styles.colorGrey5,
     bgClass: styles.backgroundGrey5
   },
   {
-    label: 'Grey #6',
+    label: 'Grey 6',
     classVar: 'Grey6',
     cssVar: '@saGrey6',
     textClass: styles.colorGrey6,
@@ -182,6 +220,9 @@ const colors = () => (
         <h1>
           <Text screaming>Colors and theming</Text>
         </h1>
+        <Paragraph>
+          <Text>Reference: <TextLink href="https://brand.seekasia.com/product/color-usage/">SEEK Asia brand portal</TextLink></Text>
+        </Paragraph>
       </Section>
       <Section>
         <h2>
@@ -192,6 +233,17 @@ const colors = () => (
       </Section>
       <Section>
         <Swatches theme={saSwatches} />
+      </Section>
+      <hr />
+      <Section>
+        <h2>
+          <Text strong yelling>
+            Blue Shades
+          </Text>
+        </h2>
+      </Section>
+      <Section>
+        <Swatches theme={blueSwatches} />
       </Section>
       <hr />
       <Section>
