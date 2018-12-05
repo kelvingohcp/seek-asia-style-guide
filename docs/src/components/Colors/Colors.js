11bfd770a6ee2a5faf3be8d4cf07227a0436643e
Patrick Liu <patrickliu@seekasia.com>
2018-12-05 10:25:20 +0800

    style(color): Update colors in style guide color page (#271)

--- docs/src/components/Colors/Colors.js
+++ docs/src/components/Colors/Colors.js
@@ -15,59 +15,57 @@ import PropTypes from 'prop-types';
 const saSwatches = [
   {
     label: 'SEEK Asia accent',
-    classVar: styles.SAAccent,
     cssVar: '@saBlue',
-    textClass: styles.colorSAAccent,
-    bgClass: styles.backgroundSAAccent
+    textClass: styles.colorSaBlue,
+    bgClass: styles.backgroundSaBlue
   },
   {
     label: 'Button, Hyperlinks',
-    classVar: styles.Button,
     cssVar: '@actionBlue',
     textClass: styles.colorButton,
     bgClass: styles.backgroundButton
   },
   {
     label: 'Completion, Success, Positive',
-    classVar: styles.Completion,
     cssVar: '@actionGreen',
     textClass: styles.colorCompletion,
     bgClass: styles.backgroundCompletion
   },
   {
     label: 'Alert',
-    classVar: styles.Alert,
     cssVar: '@actionRed',
     textClass: styles.colorAlert,
     bgClass: styles.backgroundAlert
   },
   {
     label: 'Highlight',
-    classVar: styles.Highlight,
     cssVar: '@actionPastelBlue',
     textClass: styles.colorHighlight,
     bgClass: styles.backgroundHighlight
+  },
+  {
+    label: 'Visited',
+    cssVar: '@saVisitedLink',
+    textClass: styles.colorVisited,
+    bgClass: styles.backgroundVisited
   }
 ];
 
 const dbSwatches = [
   {
     label: 'Candidate accent',
-    classVar: 'DbCandi',
     cssVar: '@dbBlue',
     textClass: styles.colorDbCandi,
     bgClass: styles.backgroundDbCandi
   },
   {
     label: 'Hirer accent',
-    classVar: 'DbHirer',
     cssVar: '@dbHirer',
     textClass: styles.colorDbHirer,
     bgClass: styles.backgroundDbHirer
   },
   {
     label: 'Call-to-Action',
-    classVar: 'DbCTA',
     cssVar: '@actionOrange',
     textClass: styles.colorDbCTA,
     bgClass: styles.backgroundDbCTA
@@ -77,21 +75,18 @@ const dbSwatches = [
 const jsSwatches = [
   {
     label: 'Candidate accent',
-    classVar: 'JsCandi',
     cssVar: '@jsBlue',
     textClass: styles.colorJsCandi,
     bgClass: styles.backgroundJsCandi
   },
   {
     label: 'Hirer accent',
-    classVar: 'JsHirer',
     cssVar: '@jsHirerGreen',
     textClass: styles.colorJsHirer,
     bgClass: styles.backgroundJsHirer
   },
   {
     label: 'Call-to-Action',
-    classVar: 'JsCTA',
     cssVar: '@actionYellow',
     textClass: styles.colorJsCTA,
     bgClass: styles.backgroundJsCTA
@@ -101,35 +96,30 @@ const jsSwatches = [
 const blueSwatches = [
   {
     label: 'Blue 1',
-    classVar: 'Blue1',
     cssVar: '@saBlue1',
     textClass: styles.colorBlue1,
     bgClass: styles.backgroundBlue1
   },
   {
     label: 'Blue 2',
-    classVar: 'Blue2',
     cssVar: '@saBlue2',
     textClass: styles.colorBlue2,
     bgClass: styles.backgroundBlue2
   },
   {
     label: 'Blue 3',
-    classVar: 'Blue3',
     cssVar: '@saBlue3',
     textClass: styles.colorBlue3,
     bgClass: styles.backgroundBlue3
   },
   {
     label: 'Blue 4',
-    classVar: 'Blue4',
     cssVar: '@saBlue4',
     textClass: styles.colorBlue4,
     bgClass: styles.backgroundBlue4
   },
   {
     label: 'Blue 5',
-    classVar: 'Blue5',
     cssVar: '@saBlue5',
     textClass: styles.colorBlue5,
     bgClass: styles.backgroundBlue5
@@ -139,56 +129,48 @@ const blueSwatches = [
 const greySwatches = [
   {
     label: 'Black',
-    classVar: 'Black',
     cssVar: '@saBlack',
     textClass: styles.colorBlack,
     bgClass: styles.backgroundBlack
   },
   {
     label: 'Grey 1',
-    classVar: 'Grey1',
     cssVar: '@saGrey1',
     textClass: styles.colorGrey1,
     bgClass: styles.backgroundGrey1
   },
   {
     label: 'Grey 2',
-    classVar: 'Grey2',
     cssVar: '@saGrey2',
     textClass: styles.colorGrey2,
     bgClass: styles.backgroundGrey2
   },
   {
     label: 'Grey 3',
-    classVar: 'Grey3',
     cssVar: '@saGrey3',
     textClass: styles.colorGrey3,
     bgClass: styles.backgroundGrey3
   },
   {
     label: 'Grey 4',
-    classVar: 'Grey4',
     cssVar: '@saGrey4',
     textClass: styles.colorGrey4,
     bgClass: styles.backgroundGrey4
   },
   {
     label: 'Grey 5',
-    classVar: 'Grey5',
     cssVar: '@saGrey5',
     textClass: styles.colorGrey5,
     bgClass: styles.backgroundGrey5
   },
   {
     label: 'Grey 6',
-    classVar: 'Grey6',
     cssVar: '@saGrey6',
     textClass: styles.colorGrey6,
     bgClass: styles.backgroundGrey6
   },
   {
     label: 'White',
-    classVar: 'White',
     cssVar: '@saWhite',
     textClass: styles.colorWhite,
     bgClass: styles.backgroundWhite
--- theme/palette/palette.js
+++ theme/palette/palette.js
@@ -1,16 +1,13 @@
 // brand colors
-export const dbBlue = '#0C4B85';
-export const jsBlue = '#1C3F94';
 export const saBlue = '#0D3880';
 
-// Action colors
-export const actionYellow = '#FFF200';
-export const actionOrange = '#FF9000';
-export const actionBlue = '#298EB9';
-export const actionGreen = '#63B209';
-export const actionRed = '#FF4B4B';
-export const actionPastelBlue = '#DDF1FA';
-export const actionTurquoise = '#0E7E8B';
+export const dbBlue = '#0C4B85';
+export const dbHirer = '#0e7e8b';
+export const dbCTA = '#FF9000';
+export const jsBlue = '#1C3F94';
+export const jsHirer = '#1B953E';
+export const jsHirerGreen = jsHirer; // Alias
+export const jsCTA = '#FFF200';
 
 // Neutrals
 export const saBlack = '#000000';
@@ -19,16 +16,26 @@ export const saGrey2 = '#666666';
 export const saGrey3 = '#999999';
 export const saGrey4 = '#CCCCCC';
 export const saGrey5 = '#EEEEEE';
+export const saGrey6 = '#f8f8fa';
 export const saWhite = '#FFFFFF';
 
 // Blue shades added at 16 Aug 2018
 // Please refer to https://brand.seekasia.com/product/color-usage/
 export const saBlue1 = '#206E8F';
-export const saBlue2 = '#298EB9'; // equivalent to "actionBlue"
+export const saBlue2 = '#298EB9';
 export const saBlue3 = '#94C9E0';
-export const saBlue4 = '#DDF1FA'; // equivalent to "actionPastelBlue"
+export const saBlue4 = '#DDF1FA';
 export const saBlue5 = '#EEF8FC';
 
-export const linkBlue = '#2E4191';
-export const panelPink = '#FF0072';
-export const lightBlue = '#EEF8FC';
+export const saVisitedLink = '#6c488f';
+
+// Action colors
+export const actionYellow = jsCTA;
+export const actionOrange = dbCTA;
+export const actionBlue = saBlue2; // Alias, equivalent to "saBlue2"
+export const actionGreen = '#63B209';
+export const actionRed = '#FF4B4B';
+export const actionPastelBlue = '#DDF1FA'; // Alias, equivalent to "saBlue4"
+export const actionTurquoise = '#0E7E8B';
+
+export const lightBlue = '#EEF8FC'; // Alias, equivalent to "saBlue5"
