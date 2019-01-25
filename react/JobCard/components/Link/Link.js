0ff274b568e387f47941a336abc7eebc75da89fb
CM Ngan <cmngan@seekasia.com>
2019-01-25 09:57:44 +0800

    fix(JobCard): remove passing non recognised property to Link

--- react/JobCard/components/Link/Link.js
+++ react/JobCard/components/Link/Link.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Link = ({ link, children, ...restProps }) => {
+const Link = ({ link, payload, linkType, children, ...restProps }) => {
   return (<a href={link} {...restProps}>{children}</a>);
 };
 
