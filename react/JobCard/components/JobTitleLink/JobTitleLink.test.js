52ab0d5116d88de33e1be5397d7ebce63a25af57
Jun Kit <jkchan@seekasia.com>
2019-08-06 13:57:38 +0800

    Task/reduce node job title module (#430)
    * fix(semanticRelease): fixing semantic release
    
    * feat(reduceNode): Reduce job title node in job card
    
    * fix(unitTest): correction on the unit test
    
    * feat(unitTest): add another unit test
    
    * fix(unitTest): update unit test
    
    * Externalise job title part
    
    * feat(JobTitle): Merge with master and some update

--- react/JobCard/components/JobTitleLink/JobTitleLink.test.js
+++ react/JobCard/components/JobTitleLink/JobTitleLink.test.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import JobTitleLink from './JobTitleLink';
+import JobTitleLink, { JobTitlePartForTest as JobTitlePart } from './JobTitleLink';
 
 const defaultJob = {
-  jobTitle: 'Software Engineer',
+  jobTitle: 'Software Engineer Richmond',
   jobUrl: '/jobCard'
 };
 
 describe('JobCard - JobTitleLink', () => {
-  it('should render correctly', () => {
+  it('should render without keywords', () => {
     const wrapper = shallow(<JobTitleLink job={defaultJob} />);
     expect(wrapper).toMatchSnapshot();
   });
-  it('should bold keyword', () => {
-    const wrapper = shallow(<JobTitleLink job={defaultJob} keyword="Seek" />);
+
+  it('should not bold anything if keywords do not match', () => {
+    const wrapper = shallow(<JobTitleLink job={defaultJob} keyword="Programmer" />);
     expect(wrapper).toMatchSnapshot();
   });
-  it('should render LinkComponent when it is provided', () => {
+
+  it('should bold matching keywords', () => {
+    const wrapper = shallow(<JobTitleLink job={defaultJob} keyword="Soft Eng" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render with LinkComponent', () => {
     const wrapper = shallow(
       <JobTitleLink
         job={defaultJob}
-        keyword="Seek"
+        keyword="Engineer"
         LinkComponent={({ link, children, ...restProps }) => (
           <span {...restProps}>{children}</span>
         )}
@@ -30,8 +37,20 @@ describe('JobCard - JobTitleLink', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should change className for viewed job', () => {
+  it('should render with viewed flag', () => {
     const wrapper = shallow(<JobTitleLink job={defaultJob} viewed />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('JobTitlePart', () => {
+    it('should render a job title part', () => {
+      const wrapper = shallow(<JobTitlePart>foo</JobTitlePart>);
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should render a highlighted job title part', () => {
+      const wrapper = shallow(<JobTitlePart isHighlighted>foo</JobTitlePart>);
+      expect(wrapper).toMatchSnapshot();
+    });
   });
 });
